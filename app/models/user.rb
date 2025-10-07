16fb159e8419399753aec10e76352b14c111dedc
Ubuntu <ubuntu@ip-172-31-29-110.ap-northeast-1.compute.internal>
2025-10-07 15:03:03 +0000

    [Add] レビュー結果の反映

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
+  
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
